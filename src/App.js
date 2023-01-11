cd2794367c2830e2c8ce825927e47968c0ca7506
Shivam Chandra <chandrshivam@gmail.com>
2023-01-11 18:07:49 +0530

    final

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Avatar from "./Components/avatar";
 function App() {
   return (
     <section className="App px-[5%] py-[2%]  md:px-[10%] md:py-[6%] lg:px-24 lg:py-10 bg-[lightgray] h-screen flex flex-col items-center">
-      <h1 className=" text-base font-bold lg:text-5xl lg:text-[4rem] lg:text-left ">
+      <h1 className="main-heading text-base font-bold lg:text-5xl lg:text-[4rem] lg:text-left sm:text-[3rem]">
         ALPACA GENERATOR
       </h1>
       <Avatar />
